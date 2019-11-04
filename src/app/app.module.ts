ac2e82cf31d77be146b7d1fe2dfb18a5369cde37
jajtuco <jajtome@gmail.com>
2019-11-04 12:51:23 -0300

    Modificaciones en los modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,37 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 //base
 import { LoginComponent } from './login/login.component';
-//shared
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { BreadcrumsComponent } from './shared/breadcrums/breadcrums.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { HeaderComponent } from './shared/header/header.component';
-//pages
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { ProgressComponent } from './pages/progress/progress.component';
+import { RegisterComponent } from './login/register.component';
 
 // Rutas 
 import { APP_ROUTE } from './app.routes';
-import { PagesComponent } from './pages/pages.component';
-import { RegisterComponent } from './login/register.component';
+
+// Modulos
+import { PagesModule } from './pages/pages.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumsComponent,
-    PagesComponent,
     RegisterComponent
+
   ],
   imports: [
     BrowserModule, 
+    PagesModule,
     APP_ROUTE
   ],
   providers: [],
